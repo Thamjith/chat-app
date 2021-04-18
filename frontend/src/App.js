80ed6513951984fa6f8b442b0ce7d3f25115e254
Thamjith <jithuthaha8@gmail.com>
2021-04-18 21:33:53 +0530

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <h1>chatapp</h1>
+     {/* Sidebar */}
+     {/* Chat Component */}
+    </div>
+  );
+}
+
+export default App;
