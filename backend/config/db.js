7825848fc3752357157ac7cbec6dbbcb3d5446fc
Thamjith <jithuthaha8@gmail.com>
2021-04-22 22:07:08 +0530

    Database Connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
-const connection_url = 'mongodb://143.110.179.176:27017/chatApp?retryWrites=true&w=majority';
-
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(connection_url, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
+        // console.log(process.env.MONGO_URI)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1)
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
 import connectDB from './config/db.js'
+import messageContent from './model/messagesModel.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 9000
 
+
 connectDB();
 
+app.use(express.json())
+
 app.get('/', (req, res)=>res.status(200).send('hello world'));
+
+app.post('/api/messages/new', (req, res) => {
+    const dbMessage = req.body
+    messageContent.create(dbMessage, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(201).send(data);
+        }
+    })
+})
 
 app.listen(port, ()=>console.log(`Listening on localhost:${port}`))
